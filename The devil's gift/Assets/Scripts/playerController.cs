40af81c9d691c399e14c9aae2fae1d4279b78013
dmirtyK <dmitrykuznetsov328@gmail.com>
2025-11-02 17:35:08 +0300

    move animation

--- The devil's gift/Assets/Scripts/playerController.cs
+++ The devil's gift/Assets/Scripts/playerController.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class playerController : MonoBehaviour
 {
-    public Transform hit;
+    private Animator animator;
 
     private bool lookRight;
 
@@ -18,12 +18,16 @@ public class playerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); // получаем компонент Rigidbody2D
+        animator = GetComponent<Animator>();
     }
 
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+            Attack();
 
+        
     }
     void FixedUpdate()
     {
@@ -31,6 +35,29 @@ public class playerController : MonoBehaviour
         moveInputV = Input.GetAxis("Vertical"); // присваеваем систему инпутов встроенных в юнити по виртикали
         rb.linearVelocity = new Vector2(moveInputV * speed, rb.linearVelocity.y); // перемещение в заданную сторону (moveInputV) помноженное на скорость (speed) в заданном векторе времени (rb.linearVelocity.y) (y напровление вектора (вертикальное))
         rb.linearVelocity = new Vector2(moveInputH * speed, rb.linearVelocity.x); // перемещение в заданную сторону (moveInputH) помноженное на скорость (speed) в заданном векторе времени (rb.linearVelocity.x) (x напровление вектора (горизонтальное))
+
+        if (moveInputH == 0)
+            animator.SetBool("IsMovingX", false);
+        else
+            animator.SetBool("IsMovingX", true);
+
+        if (moveInputV == 0)
+        {
+            animator.SetBool("IsMovingY-", false);
+            animator.SetBool("IsMovingY+", false);
+        }    
+        else if (moveInputV > 0)
+        {
+            animator.SetBool("IsMovingY-", false);
+            animator.SetBool("IsMovingY+", true);
+        }
+        else if (moveInputV < 0)
+        {
+            animator.SetBool("IsMovingY-", true);
+            animator.SetBool("IsMovingY+", false);
+        }
+
+
 
         if (lookRight == true && moveInputH > 0) // условия поворота 
         {
@@ -50,9 +77,8 @@ public class playerController : MonoBehaviour
         transform.localScale = Scaler;
     }
 
-    private void OnMouseDown()
+    public void Attack()
     {
-        Instantiate(hit, transform.position, transform.rotation);
-       
+
     }
 }
