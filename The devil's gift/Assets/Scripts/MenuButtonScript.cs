286ce3bee8b386d297531b8beb7092a848185f52
Bizarui <abuziarovmark@gmail.com>
2025-11-02 20:54:40 +0300

    Красотища та какая

---
+++ The devil's gift/Assets/Scripts/MenuButtonScript.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
